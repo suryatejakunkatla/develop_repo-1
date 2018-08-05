7ece6f385aac7fcbe05fd51728f281c75ebb11d7
suryatejakunkatla <42084174+suryatejakunkatla@users.noreply.github.com>
2018-08-05 10:51:57 +0530

    Update Program.cs

--- ApplicationOnGit/ApplicationOnGit/Program.cs
+++ ApplicationOnGit/ApplicationOnGit/Program.cs
@@ -11,7 +11,7 @@ namespace ApplicationOnGit
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello dgtjwertgyh- World");
+            Console.WriteLine("Hello surya- World");
             Console.ReadLine();
         }
     }
